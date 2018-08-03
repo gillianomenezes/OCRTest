638111099b054ff34e6f2cd3aa9f5ceb9cb0f1ac
Gilliano Menezes <g.silva.de.menezes@avanade.com>
2018-08-03 11:45:46 -0300

    first commit

---
+++ OCRTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OCRTest
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string skeys = ConfigurationManager.AppSettings["skeys"];
+            Console.Write(skeys);
+            Console.ReadLine();
+        }
+    }
+}
